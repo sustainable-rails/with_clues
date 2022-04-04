d3dca54111bfbbadb4bc617cbaeaceadb0585f77
Dave Copeland <davec@naildrivin5.com>
2022-04-04 09:29:18 -0400

    handle the Simple driver by falling back to native.
    View Component in particular works with Capybara's simple driver, which
    does not respond to `.html` for reasons I don't understand.  `.native`
    appears to return some Nokogiri thing which, when turned into a string,
    results in seeing the HTML, which is why you'd use this gem.

--- lib/with_clues/html.rb
+++ lib/with_clues/html.rb
@@ -1,14 +1,16 @@
 module WithClues
   class Html
     def dump(notifier, page:, context:)
-      if !page.respond_to?(:html)
-        notifier.notify "Something may be wrong. page (#{page.class}) does not respond to #html"
-        return
-      end
       notifier.blank_line
       notifier.notify "HTML {"
       notifier.blank_line
-      notifier.notify_raw page.html
+      if page.respond_to?(:html)
+        notifier.notify_raw page.html
+      elsif page.respond_to?(:native)
+        notifier.notify_raw page.native
+      else
+        notifier.notify "[!] Something may be wrong. page (#{page.class}) does not respond to #html or #native"
+      end
       notifier.blank_line
       notifier.notify "} END HTML"
     end
